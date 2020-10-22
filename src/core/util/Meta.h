184df4790c6a06bface1a782c8681cfd97ca2f2c
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-10-22 11:32:54 +0200

    Add OnlyCallableBy metaprogramming helper class.

--- src/core/util/Meta.h
+++ src/core/util/Meta.h
@@ -6,6 +6,26 @@ namespace ire::core::util {
     template <typename T>
     constexpr bool AlwaysFalseV = false;
 
+    template <typename AllowedCallerT>
+    struct OnlyCallableBy
+    {
+        friend struct AllowedCallerT;
+
+        OnlyCallableBy(const OnlyCallableBy&) = delete;
+        OnlyCallableBy(OnlyCallableBy&&) = delete;
+
+        OnlyCallableBy& operator=(const OnlyCallableBy&) = delete;
+        OnlyCallableBy& operator=(OnlyCallableBy&&) = delete;
+
+        AllowedCallerT& operator*() const { return m_caller; }
+        AllowedCallerT* operator->() const { return &m_caller; }
+
+    private:
+        OnlyCallableBy(AllowedCallerT& caller) : m_caller(caller) { }
+
+        AllowedCallerT& m_caller;
+    };
+
 }
 
 #endif // !META_H
