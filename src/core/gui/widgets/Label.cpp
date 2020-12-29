c042d1c987de686c89a017018994fa879636f4a6
Jakub Dobrowolski <jd.kuba96@gmail.com>
2020-12-29 19:12:20 +0100

    Added Basic Functionalities to allow text to appear and edit basic text attributes

--- src/core/gui/widgets/Label.cpp
+++ src/core/gui/widgets/Label.cpp
@@ -7,28 +7,192 @@ namespace ire::core::gui
 
 	Label::Label()
 	{
-		m_rectangleShape.setFillColor(sf::Color::Green);
+		setBackgroundShapeColor(sf::Color::Green);
+
+		setCharacterSize(45);
+		setTextVisible(true);
+		setTextFillColor(sf::Color::White);
+
 	}
 
 	std::unique_ptr<Label> Label::create(const std::string& text)
 	{
 		auto widget = std::make_unique<Label>();
-
+		widget->setTextString(text);
 		return widget;
 	}
 
 	void Label::draw(sf::RenderTarget& target)
 	{
-		target.draw(m_rectangleShape);
+		if (m_isBackgroundVisible)
+		{
+			target.draw(m_backgroundShape);
+		}
+		
+		if (m_mainText.isVisible())
+		{
+			target.draw(m_mainText);
+		}
 	}
 
 	void Label::updateWidget()
+	{	
+		m_mainText.setTextPosition(m_position, m_size);
+
+		m_backgroundShape.setSize(m_size);
+		m_backgroundShape.setPosition(m_position);
+
+	}
+
+	void Label::setTextString(const std::string& string)
 	{
-		//const auto size = getSize();
-		m_rectangleShape.setSize(m_size);
+		if (m_mainText.getString() != string)
+		{
+			m_mainText.setString(string);
+		}
+	}
 
-		//const auto position = getPosition();
-		m_rectangleShape.setPosition(m_position);
+	const std::string Label::getTextString() const
+	{
+		return m_mainText.getString();
+	}
 
+	void Label::setCharacterSize(unsigned int characterSize)
+	{
+		if (m_mainText.getCharacterSize() != characterSize)
+		{
+			m_mainText.setCharacterSize(characterSize);
+		}
+	}
+
+	const unsigned int Label::getCharacterSize() const
+	{
+		return m_mainText.getCharacterSize();
+	}
+
+	void Label::setVerticalAlignment(Text::VerticalAlignment verticalAlignment)
+	{
+		if (m_mainText.getVerticalAlign() != verticalAlignment)
+		{
+			m_mainText.setVerticalAlign(verticalAlignment);
+		}
+	}
+
+	const Text::VerticalAlignment Label::getVerticalAlignment() const
+	{
+		return m_mainText.getVerticalAlign();
+	}
+
+	void Label::setHorizontalAlignment(Text::HorizontalAlignment horizontalAlignment)
+	{
+		if (m_mainText.getHorizontalAlign() != horizontalAlignment)
+		{
+			m_mainText.setHorizontalAlign(horizontalAlignment);
+		}
+	}
+
+	const Text::HorizontalAlignment Label::getHorizontalAlignment() const
+	{
+		return m_mainText.getHorizontalAlign();
+	}
+
+	void Label::setTextVisible(bool isVisible)
+	{
+		if (m_mainText.isVisible() != isVisible)
+		{
+			m_mainText.setVisible(isVisible);
+		}
+	}
+
+	const bool Label::isTextVisible() const
+	{
+		return m_mainText.isVisible();
+	}
+
+	void Label::setTextFillColor(sf::Color fillColor)
+	{
+		if (m_mainText.getFillColor() != fillColor)
+		{
+			m_mainText.setFillColor(fillColor);
+		}
+	}
+
+	const sf::Color Label::getTextFillColor() const
+	{
+		return m_mainText.getFillColor();
+	}
+
+	void Label::setTextOutlineColor(sf::Color outlineColor)
+	{
+		if (m_mainText.getOutlineColor() != outlineColor)
+		{
+			m_mainText.setOutlineColor(outlineColor);
+		}
+	}
+
+	const sf::Color Label::getTextOutlineColor() const
+	{
+		return m_mainText.getOutlineColor();
+	}
+
+	void Label::setTextOutlineThickness(float outlineThickness)
+	{
+		if (m_mainText.getOutlineThickness() != outlineThickness)
+		{
+			m_mainText.setOutlineThickness(outlineThickness);
+		}
+	}
+
+	const float Label::getTextOutlineThickness() const
+	{
+		return m_mainText.getOutlineThickness();
+	}
+
+	void Label::setTextStyle(sf::Uint32 style)
+	{
+		if (m_mainText.getStyle() != style)
+		{
+			m_mainText.setStyle(style);
+		}
+	}
+
+	void Label::setTextLatterSpacing(float spacingFactor)
+	{
+		if (m_mainText.getLetterSpacing() != spacingFactor)
+		{
+			m_mainText.setLetterSpacing(spacingFactor);
+		}
+	}
+
+	void Label::setTextLineSpacing(float spacingFactor)
+	{
+		m_mainText.setLineSpacing(spacingFactor);
+	}
+
+	void Label::setTextFont(const std::string& pathToFont)
+	{
+		m_mainText.setFont(pathToFont);
+	}
+	void Label::setBackgroundShapeColor(sf::Color backgroundColor)
+	{
+		if (m_backgroundShape.getFillColor() != backgroundColor)
+		{
+			m_backgroundShape.setFillColor(backgroundColor);
+		}
+	}
+	const sf::Color Label::getBackgroundShapeColor() const
+	{
+		return m_backgroundShape.getFillColor();
+	}
+	void Label::setBackgdoungVisibility(bool isBackgroundVisible)
+	{
+		if (m_isBackgroundVisible != isBackgroundVisible)
+		{
+			m_isBackgroundVisible = isBackgroundVisible;
+		}
+	}
+	const bool Label::isBackgroundVisible() const
+	{
+		return m_isBackgroundVisible;
 	}
 }
--- src/core/gui/widgets/Label.h
+++ src/core/gui/widgets/Label.h
@@ -2,6 +2,7 @@
 #define LABEL_H
 
 #include "ClickableWidget.h"
+#include "core/gui/Text.h"
 
 namespace ire::core::gui
 {
@@ -16,13 +17,55 @@ namespace ire::core::gui
 
         void updateWidget() override;
 
+        void setTextString(const std::string& string);
+        const std::string getTextString() const;
+
+        void setCharacterSize(unsigned int characterSize);
+        const unsigned int getCharacterSize() const;
+
+        void setVerticalAlignment(Text::VerticalAlignment verticalAlignment);
+        const Text::VerticalAlignment getVerticalAlignment() const;
+
+        void setHorizontalAlignment(Text::HorizontalAlignment horizontalAlignment);
+        const Text::HorizontalAlignment getHorizontalAlignment() const;
+
+        void setTextVisible(bool isVisible);
+        const bool isTextVisible() const;
+
+        void setTextFillColor(sf::Color fillColor);
+        const sf::Color getTextFillColor() const;
+
+        void setTextOutlineColor(sf::Color outlineColor);
+        const sf::Color getTextOutlineColor() const;
+
+        void setTextOutlineThickness(float outlineThickness);
+        const float getTextOutlineThickness() const;
+
+        void setTextStyle(sf::Uint32 style);
+
+        void setTextLatterSpacing(float spacingFactor);
+
+        void setTextLineSpacing(float spacingFactor);
+
+        void setTextFont(const std::string& pathToFont);
+
+        void setBackgroundShapeColor(sf::Color backgroundColor);
+        const sf::Color getBackgroundShapeColor() const;
+
+        void setBackgdoungVisibility(bool isBackgroundVisible);
+        const bool isBackgroundVisible() const;
+
         static const WidgetType m_type;
         const WidgetType getType() const override
         {
             return m_type;
         }
     private:
-        sf::RectangleShape m_rectangleShape;
+        sf::RectangleShape m_backgroundShape;
+        bool m_isBackgroundVisible = false;
+
+        Text m_mainText;
+        
     };
 }
 #endif // !LABEL_H
