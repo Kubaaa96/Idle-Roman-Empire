3c265442f98c74adefd8df99437e1b68dca41734
Tomasz Sobczyk <tomasz.sobczyk1997@gmail.com>
2020-12-30 19:40:57 +0100

    Move WorldView to separate file.

--- src/client/states/IntroState.cpp
+++ src/client/states/IntroState.cpp
@@ -9,110 +9,6 @@
 
 namespace ire::client::state
 {
-    struct WorldView : ire::core::gui::ClickableWidget
-    {
-    private:
-        enum struct State
-        {
-            Inactive,
-            Active
-        };
-
-    public:
-
-        WorldView() :
-            m_world(std::make_unique<ire::core::world::TiledTopDownSurface>(512, 512)),
-            m_state(State::Inactive)
-        {
-        }
-
-        static std::unique_ptr<WorldView> create()
-        {
-            auto widget = std::make_unique<WorldView>();
-            return widget;
-        }
-
-        void draw(sf::RenderTarget& target)
-        {
-            updateCamera();
-
-            sf::RenderStates states = sf::RenderStates::Default;
-            sf::View view = target.getView();
-            view.setViewport(sf::FloatRect(0.1, 0.1, 0.8, 0.8));
-            target.setView(view);
-            sf::RectangleShape s;
-            s.setFillColor(sf::Color::White);
-            s.setSize(sf::Vector2f(1000.0f, 1000.0f));
-            target.draw(s);
-            m_world.getMainSurface().draw(target, states);
-        }
-
-        static const WidgetType m_type;
-        const WidgetType getType() const override
-        {
-            return m_type;
-        }
-
-        // Why the hell is this virtual?
-        void updateWidget() override {}
-
-        void onEvent(core::gui::EventRoot& sender, core::gui::MouseButtonDownEvent& ev)
-        {
-            m_state = State::Active;
-            sender.setActiveWidget(*this);
-            ev.handled = true;
-        }
-
-        void onStoppedBeingActive() override
-        {
-            m_state = State::Inactive;
-        }
-
-    private:
-        ire::core::world::World m_world;
-        State m_state;
-
-        void updateCamera()
-        {
-            // TODO: replace later with event based and framerate agnostic implementation.
-
-            auto& mainSurface = static_cast<ire::core::world::TiledTopDownSurface&>(m_world.getMainSurface());
-            auto zoom = mainSurface.getZoom();
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-            {
-                mainSurface.moveCamera(sf::Vector2f(-1.0f, 0.0f) / zoom);
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-            {
-                mainSurface.moveCamera(sf::Vector2f(1.0f, 0.0f) / zoom);
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-            {
-                mainSurface.moveCamera(sf::Vector2f(0.0f, -1.0f) / zoom);
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-            {
-                mainSurface.moveCamera(sf::Vector2f(0.0f, 1.0f) / zoom);
-            }
-            
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Add))
-            {
-                mainSurface.changeZoom(1.01f);
-            }
-
-            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Subtract))
-            {
-                mainSurface.changeZoom(1 / 1.01f);
-            }
-        }
-    };
-
-    const WidgetType WorldView::m_type = WidgetType::create<WorldView>("WorldView");
-
     IntroState::IntroState(core::state::StateMachine& stateMachine, core::gui::SystemWindow& window, bool replace)
         : State { stateMachine, window, replace}
     {
@@ -140,11 +36,11 @@ namespace ire::client::state
         auto vLayoutMain = ire::core::gui::VerticalLayout::create(currentSizeOfViewPort);
 
         vLayoutMain->add(std::move(labelIntroText), "labelIntroText");
-        //vLayoutMain->add(std::move(buttonContinue), "buttonContinue");
+        vLayoutMain->add(std::move(buttonContinue), "buttonContinue");
 
-        //vLayoutMain->setMargins({ 50, 50, 50, 50 });
-        //vLayoutMain->setSpaces( 20 );
-        //vLayoutMain->setLayoutStretch({ 3, 1 });
+        vLayoutMain->setMargins({ 50, 50, 50, 50 });
+        vLayoutMain->setSpaces( 20 );
+        vLayoutMain->setLayoutStretch({ 3, 1 });
 
         m_group = ire::core::gui::Group::create(currentSizeOfViewPort);
         m_group->add(std::move(vLayoutMain), "layoutMain");
